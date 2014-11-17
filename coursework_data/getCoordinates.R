988806fee5f4721fd28fbbd2db4478b5eb93672a
roddy <rodrigopracana@gmail.com>
2014-11-17 16:58:48 +0000

    added getCoordinates() function and the data necessary to run it

--- coursework_data/getCoordinates.R
+++ coursework_data/getCoordinates.R
@@ -1,8 +1,8 @@
 getCoordinates <- function() {
 
   # Download and read the tables
-  table1 <- read.csv('http://bmpvieira.github.io/temp/alabamaants-myrmicinaepheidole_obscurithorax.csv', h=T)
-  table2 <- read.csv('http://bmpvieira.github.io/temp/allantwebants-myrmicinaesolenopsis_invicta.csv', h=T)
+  table1 <- read.csv('https://github.com/wurmlab/2014-MSc-SBSm032-stats_bioinformatics/tree/coursework/coursework_data/alabamaants-myrmicinaepheidole_obscurithorax.csv', h=T)
+  table2 <- read.csv('https://github.com/wurmlab/2014-MSc-SBSm032-stats_bioinformatics/tree/coursework/coursework_data/allantwebants-myrmicinaesolenopsis_invicta.csv', h=T)
 
   # Merge the two tables
   # Subset the table to a new one with only the rows that match the Genus provided by the argument of the function

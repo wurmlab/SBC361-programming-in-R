1710b8f54658f65199b44b4d1191b9db1d70614f
roddy <rodrigopracana@gmail.com>
2014-11-17 19:30:50 +0000

    changed getCoordinates.R to have the right data paths

--- coursework/getCoordinates.R
+++ coursework/getCoordinates.R
@@ -1,8 +1,8 @@
 getCoordinates <- function() {
 
   # Download and read the tables
-  table1 <- read.csv('https://github.com/wurmlab/2014-MSc-SBSm032-stats_bioinformatics/tree/coursework/coursework_data/alabamaants-myrmicinaepheidole_obscurithorax.csv', h=T)
-  table2 <- read.csv('https://github.com/wurmlab/2014-MSc-SBSm032-stats_bioinformatics/tree/coursework/coursework_data/allantwebants-myrmicinaesolenopsis_invicta.csv', h=T)
+  table1 <- read.csv('http://wurmlab.github.io/2014-MSc-SBSm032-stats_bioinformatics/coursework/data/allantwebants-myrmicinaesolenopsis_invicta.csv', h=T)
+  table2 <- read.csv('http://wurmlab.github.io/2014-MSc-SBSm032-stats_bioinformatics/coursework/data/alabamaants-myrmicinaepheidole_obscurithorax.csv', h=T)
 
   # Merge the two tables
   # Subset the table to a new one with only the rows that match the Genus provided by the argument of the function

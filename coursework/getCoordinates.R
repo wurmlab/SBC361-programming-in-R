27bf4c644b6bcfbbc52aa3cef046a9be4d79a857
Yannick Wurm <y.wurm@qmul.ac.uk>
2014-11-18 10:31:09 +0000

    more human-friendly variable names
    Respect the style guide http://r-pkgs.had.co.nz/style.html

--- coursework/getCoordinates.R
+++ coursework/getCoordinates.R
@@ -1,8 +1,9 @@
-getCoordinates <- function() {
+get_coordinates <- function() {
 
   # Download and read the tables
-  table1 <- read.csv('http://wurmlab.github.io/2014-MSc-SBSm032-stats_bioinformatics/coursework/data/allantwebants-myrmicinaesolenopsis_invicta.csv', h=T)
-  table2 <- read.csv('http://wurmlab.github.io/2014-MSc-SBSm032-stats_bioinformatics/coursework/data/alabamaants-myrmicinaepheidole_obscurithorax.csv', h=T)
+  data_dir <- 'http://wurmlab.github.io/2014-MSc-SBSm032-stats_bioinformatics/coursework/data/'
+  solenopsis_invicta_collections     <- read.csv(file.path(data_dir, 'allantwebants-myrmicinaesolenopsis_invicta.csv'),   h=T)
+  pheidole_obscurithorax_collections <- read.csv(file.path(data_dir, 'alabamaants-myrmicinaepheidole_obscurithorax.csv'), h=T)
 
   # Merge the two tables
   # Subset the table to a new one with only the rows that match the Genus provided by the argument of the function

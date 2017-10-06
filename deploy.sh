addae673c5ee15ac4e5020d9cea15cf47e8d61cc
Carlos Martinez-Ruiz <c.martinezruiz@qmul.ac.uk>
2017-10-06 10:57:00 +0100

    Update for 2017 practicals

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -o errexit -o nounset
 
 rev=$(git rev-parse --short HEAD)
 
-cd 2016
+cd 2017
 # RUN PANDOC and create HTML
 for i in *md; do
   if [[ $i != 'README.md' ]]; then
@@ -18,8 +18,8 @@ cd public
 git init
 
 
-git config user.name "lrsantiago"
-git config user.email "l.santiago@qmul.ac.uk"
+git config user.name "MartinezRuiz-Carlos"
+git config user.email "c.martinezruiz@qmul.ac.uk"
 git remote add deploy "https://$GH_TOKEN@github.com/wurmlab/SBC361-programming-in-R.git"
 
 git fetch deploy gh-pages

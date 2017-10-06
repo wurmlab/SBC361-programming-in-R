e7a69a50b4dbc53a9dd08830982a5149c53938bc
Anurag Priyam <anurag.priyam@qmul.ac.uk>
2017-10-06 15:42:08 +0100

    Auto-deploy similar to genomicscourse
    Signed-off-by: Anurag Priyam <anurag.priyam@qmul.ac.uk>

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,45 @@
 #!/bin/bash
+# From https://gist.github.com/bmpvieira/ae0d9f9e6cff3df831cc5b6511207138 and modified
+set -e # Exit with nonzero exit code if anything fails
 
-set -o errexit -o nounset
+SOURCE_BRANCH="master"; TARGET_BRANCH="gh-pages"
 
-rev=$(git rev-parse --short HEAD)
+function doCompile {
+  ./compile.sh
+}
 
-cd 2017
-# RUN PANDOC and create HTML
-for i in *md; do
-  if [[ $i != 'README.md' ]]; then
-    pandoc -f markdown_github -c ../theme.css -s $i -o ../public/${i%%md}html;
-  fi
-done
-cd ..
+echo "Pull requests and commits to other branches shouldn't try to deploy, just build to verify"
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo "Skipping deploy; just doing a build."
+    doCompile
+    exit 0
+fi
 
+echo "Save some useful information"
+REPO_URL="https://$GH_TOKEN@github.com/wurmlab/SBC361-programming-in-R.git"
+SHA=`git rev-parse --verify HEAD`
+
+echo "Run our compile script"
+doCompile
+
+echo "Clone the existing gh-pages for this repo into public/"
 cd public
+git remote add deploy $REPO_URL
+git fetch deploy $TARGET_BRANCH
+git reset deploy/$TARGET_BRANCH
 
-git init
-git remote add deploy "https://github.com/wurmlab/SBC361-programming-in-R.git"
-git fetch deploy gh-pages
-git reset deploy/gh-pages
+echo "If there are no changes to the compiled out (e.g. this is a README update) then just bail."
+if [[ -z `git diff --exit-code` ]]; then
+    echo "No changes to the output on this push; exiting."
+    exit 0
+fi
 
-git add -A .
-git commit -m "build website at ${rev}"
+echo 'Commit the "changes", i.e. the new version.'
+echo "The delta will show diffs between new and old versions."
+git add --all .
+git config user.name "lrsantiago"
+git config user.email "l.santiago@qmul.ac.uk"
+git commit -m "Deploy to GitHub Pages: ${SHA}"
+
+echo "Now that we're all set up, we can push."
 git push deploy HEAD:gh-pages

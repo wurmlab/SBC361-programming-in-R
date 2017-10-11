0bb0ee770f378f2019068dde8514b41be2d84ef0
Anurag Priyam <anurag.priyam@qmul.ac.uk>
2017-10-11 09:56:05 +0100

    Website - header, footer, and CSS
    Signed-off-by: Anurag Priyam <anurag.priyam@qmul.ac.uk>

--- compile.sh
+++ compile.sh
@@ -8,7 +8,8 @@ cd $year
 echo "Converting markdown files in $year/ to html"
 for i in *md; do
   if [[ $i != 'README.md' ]]; then
-    pandoc -f markdown_github -c ../theme.css -s $i -o ../public/${i%%md}html;
+    pandoc -s $i -f markdown_github -c github-pandoc.css -B ../header.html \
+    -A ../footer.html -o ../public/${i%%md}html
   fi
 done
 cd ..
